ec12cedab56e87b2f47cb6d7b2e5334300496cec
A Samod <asamod999@gmail.com>
2026-06-30 22:33:59 +0530

    Update base path in Vite configuration for live deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/portfolio-builder/',
+    base: '/live-portfolio-builder/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
